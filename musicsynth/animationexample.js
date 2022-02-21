fb38eff118330ff8ed300e924a69066a14acf2dd
Visionist <escaperecovery@gmail.com>
2022-02-20 16:06:09 -0800

    Delete musicsynth directory

--- musicsynth/animationexample.js
+++
@@ -1,9 +0,0 @@
-var x = 0;//0-100% of the screen width
-var y = 0;
-var angle = 0;//0-360 degrees
-
-
-
-document.getElementById("obs").style.left=x+"%";
-         document.getElementById("obs").style.top=y+"%";
-         document.getElementById("obs").style.transform="translate(-50%,-50%) rotate("+angle+"deg)";
